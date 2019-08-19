fbe35387180a11d419d350649218253fa189d248
allahyar <aallahparast@gmail.com>
2019-08-19 19:02:07 +0430

    add base class for components

---
+++ projects/core/src/lib/classes/base.class.ts
@@ -0,0 +1,15 @@
+import {OnDestroy, OnInit} from '@angular/core';
+
+export abstract class Base implements OnInit, OnDestroy {
+
+  isAlive = true;
+
+  ngOnInit(): void {
+  }
+
+  // .pipe(takeWhile(x => this.isAlive))
+
+  ngOnDestroy(): void {
+    this.isAlive = false;
+  }
+}
