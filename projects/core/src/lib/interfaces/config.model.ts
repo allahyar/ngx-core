25a27c815f5b679a28887276d57b49b38b71266d
allahyar <aallahparast@gmail.com>
2019-08-26 18:04:12 +0430

    change tslint & add 3 dependency

--- projects/core/src/lib/interfaces/config.model.ts
+++ projects/core/src/lib/interfaces/config.model.ts
@@ -1,31 +1,48 @@
+import {NgxCoolDialogTheme} from 'ngx-cool-dialogs';
+
 export interface CoreModuleConfig {
-  storage?: {
-    prefix: string
-  };
-  language?: {
-    default: string;
-    support: string[];
-  };
+	storage?: {
+		prefix: string
+	};
+	language?: {
+		default: string;
+		support: string[];
+	};
 }
 
 export interface AuthModuleConfig {
-  loginEndPoint?: string;
-  guards?: {
-    loggedInGuard: {
-      redirectUrl: string;
-    };
-    loggedOutGuard: {
-      redirectUrl: string;
-    };
-  };
+	loginEndPoint: string;
+	guards: {
+		loggedInGuard: {
+			redirectUrl: string;
+		};
+		loggedOutGuard: {
+			redirectUrl: string;
+		};
+	};
+}
+
+export interface UiModuleConfig {
+	dialog: {
+		theme?: NgxCoolDialogTheme;
+		titles?: {
+			alert?: string;
+			confirm?: string;
+			prompt?: string;
+		} | string;
+		defaultText?: string;
+		okButtonText: string;
+		cancelButtonText: string;
+		color: string;
+	};
 }
 
 export interface LoginParams {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 
 export interface LocalStorageConfig {
-  prefix?: string;
+	prefix?: string;
 }
 
